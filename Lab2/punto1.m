90165120dfac54c3b126d916d3d0359aebc6ae9e
Kevin Andrey Herrera Silva <kahs_kevin@hotmail.com>
2020-05-05 14:24:21 -0500

    corrección

--- Lab2/punto1.m
+++ Lab2/punto1.m
@@ -1,6 +1,4 @@
-% Script que hace la fft al pulso rectangular y aplica filtros 
-
-%--
+#Modulación y demodulación PCM con cuantización uniforme
 IN= input('Ingrese la función: ', "s");
 F=inline(IN,'x');
 
@@ -8,29 +6,30 @@ n= 2 #Numero de pulsos binarios
 T_m=4*pi; #Periodo
 f_m=1/T_m; #Frecuencia 
 f_s=2*f_m; #Frecuencia de muestreo
-% Arreglo para el muestreo
+# Arreglo para el muestreo
 t=[0:f_s:2*T_m];
-% Arreglo función original
-t_s=[0:0.02:2*T_m];
+# Arreglo función original
+#t_s=[0:0.02:2*T_m];
+t_s=[0:f_s:2*T_m];
 
-%Cuantizacion uniforme
+#Cuantizacion uniforme
 x=F(t);
 xq=cuantUniforme(x,1,n);
 
-%-----Literal a. MUESTREO-----------
+%MUESTREO APLICANDO TEOREMA DE MUESTREO
 
 figure(1);
 subplot(3,1,1),plot(t_s,F(t_s),'k'); title('F(t) original'); grid;
 subplot(3,1,2),stem(t,F(t),'k'); title('F(t) muestreada'); grid;
 
 
-%------Literal b. CUANTIZACION-------
+#CUANTIZACIÓN, UTILIZANDO CUANTIZACIÓN UNIFORME
 
 subplot(3,1,3);stem(t,xq,'k'); title('F(t) cuantizada');xlabel('nT_s'); ylabel('x(nT_s)');
 
 
 
-%------Literal c. CODIFICACION--------
+#CODIFICACIÓN
 
 pulsos_binarios = log2(n); #log_2 (n) pulsos binarios por cada muestreo de la senal
 num_total_pulsos = length(F(t))*pulsos_binarios; #Cantidad total de pulsos binarios necesarios para transmitir la senal
@@ -42,7 +41,7 @@ y_niveles_binario
 
 aaa=1
 while (aaa==1)
-  %-------Literal d. REPRESENTACION DEL PULSO-------------------------------
+  #GENERACIÓN DE FORMATOS DE SEÑALIZACIÓN
   disp("Seleccione el tipo de codificación :");
   disp("1-Unipolar NRZ");
   disp("2-Bipolar NRZ");
@@ -140,7 +139,7 @@ while (aaa==1)
   t1=(0:(length(y)-1))/f_s;
   figure(2);
   subplot(1,1,1);plot(t1,y,'k');axis([0 100 -1.1 1.1]); title('Señal codificada');xlabel('nT_s'); ylabel('x(nT_s)');
-  %-------Literal e. RECUPERAR M(T)---------
+  #RECUPERACIÓN DE LA SEÑAL (DEMOLUDADOR)
 
   sum=0;
   w_m=2*pi*f_m;
